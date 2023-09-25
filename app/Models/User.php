f5af57f638392cee74e12b7398038878b443a000
SenadK1997 <senad.okt97@gmail.com>
2023-09-25 23:19:07 +0200

    domain edited

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,6 @@ class User extends Authenticatable
     ];
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@balkanpedia.com') && $this->hasVerifiedEmail();
     }
 }
