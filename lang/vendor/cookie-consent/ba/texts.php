8e8daa6fbe58281bc33d5571d73a6a7215760f80
SenadK1997 <senad.okt97@gmail.com>
2023-09-27 12:55:23 +0200

    cookies notice

---
+++ lang/vendor/cookie-consent/ba/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'Vaše iskustvo na ovoj stranici će se poboljšati omogućavanjem kolačića.',
+    'agree' => 'Dozvoli kolačiće',
+];
---
+++ lang/vendor/cookie-consent/en/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'By visiting our site, you agree to accept our privacy and policy.',
+    'agree' => 'Close',
+];
